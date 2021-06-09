10176510b94f7932082eea5dee09c8c396e30406
wyzhang <wyzhang@google.com>
2021-06-09 12:04:17 -0700

    CDAP-18045: Encrypt user credential at router and decrypt before call auth extension

--- cdap-gateway/src/main/java/io/cdap/cdap/gateway/router/handlers/AuthenticationHandler.java
+++ cdap-gateway/src/main/java/io/cdap/cdap/gateway/router/handlers/AuthenticationHandler.java
@@ -22,8 +22,11 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import io.cdap.cdap.common.conf.CConfiguration;
 import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.common.conf.SConfiguration;
 import io.cdap.cdap.common.logging.AuditLogEntry;
 import io.cdap.cdap.common.utils.Networks;
+import io.cdap.cdap.security.auth.CipherException;
+import io.cdap.cdap.security.auth.TinkCipher;
 import io.cdap.cdap.security.auth.UserIdentityExtractionResponse;
 import io.cdap.cdap.security.auth.UserIdentityExtractionState;
 import io.cdap.cdap.security.auth.UserIdentityExtractor;
@@ -74,6 +77,7 @@ public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
   private static final Logger AUDIT_LOGGER = LoggerFactory.getLogger(Constants.Router.AUDIT_LOGGER_NAME);
 
   private final CConfiguration cConf;
+  private final SConfiguration sConf;
   private final String realm;
   private final Pattern bypassPattern;
   private final boolean auditLogEnabled;
@@ -81,9 +85,11 @@ public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
   private final DiscoveryServiceClient discoveryServiceClient;
   private final UserIdentityExtractor userIdentityExtractor;
 
-  public AuthenticationHandler(CConfiguration cConf, DiscoveryServiceClient discoveryServiceClient,
+  public AuthenticationHandler(CConfiguration cConf, SConfiguration sConf,
+                               DiscoveryServiceClient discoveryServiceClient,
                                UserIdentityExtractor userIdentityExtractor) {
     this.cConf = cConf;
+    this.sConf = sConf;
     this.realm = cConf.get(Constants.Security.CFG_REALM);
     this.bypassPattern = createBypassPattern(cConf);
     this.auditLogEnabled = cConf.getBoolean(Constants.Router.ROUTER_AUDIT_LOG_ENABLED);
@@ -111,7 +117,10 @@ public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
       UserIdentityPair userIdentityPair = extractionResponse.getIdentityPair();
       // User identity extraction succeeded, so set some header properties and allow the call through
       request.headers().remove(HttpHeaderNames.AUTHORIZATION);
-      String credential = userIdentityPair.getUserCredential();
+
+      // When enabled, encrypt user credential and pass it in the encrypted form around services.
+      String credential = encryptUserCredential(userIdentityPair.getUserCredential());
+
       // For backwards compatibility, we continue propagating credentials by default. This may change in the future.
       if (cConf.getBoolean(Constants.Security.Authentication.PROPAGATE_USER_CREDENTIAL, true) &&
         credential != null) {
@@ -217,6 +226,17 @@ public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
     } finally {
       cancellable.cancel();
     }
+  }
+
+  /**
+   * Encrypt the given credential followed by a base64 encoding.
+   */
+  private String encryptUserCredential(String credential) throws CipherException {
+    if (credential == null ||
+      !sConf.getBoolean(Constants.Security.Authentication.USER_CREDENTIAL_ENCRYPTION_ENABLED, false)) {
+      return credential;
+    }
+    return new TinkCipher(sConf).encryptToBase64(credential.getBytes(StandardCharsets.UTF_8), null);
   }
 
   private void addAuthServerUrls(Iterable<Discoverable> discoverables, String protocol, int port, JsonArray result) {
